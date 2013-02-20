81bdfa88194c9aeb6f2a4a95f05ec616d0b310ee
Ryan Johnson <ryan.txanson@gmail.com>
2013-02-20 01:32:14 +0000

    seem to have fixed the mobile madness, be careful with jquery pushStateEnabled and hashListeningEnabled
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/aajege@71454 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/sma-client/vendor/scripts/jquery.mobile.init.js
+++ src/sma-client/vendor/scripts/jquery.mobile.init.js
@@ -5,14 +5,15 @@
 //     $.mobile.pushStateEnabled = false;
 // });
 $(document).bind("mobileinit", function () {
-	console.log("mobileinit");
     $.mobile.ajaxEnabled = false;
     $.mobile.linkBindingEnabled = false;
     $.mobile.hashListeningEnabled = false;
-    $.mobile.pushStateEnabled = true;
+    $.mobile.pushStateEnabled = false;
 
-    // Remove page from DOM when it's being replaced
-    $('div[data-role="page"]').live('pagehide', function (event, ui) {
-        $(event.currentTarget).remove();
+    $(document).ready(function () {
+        // Remove page from DOM when it's being replaced
+        $('div[data-role="page"]').live('pagehide', function (event, ui) {
+            $(event.currentTarget).remove();
+        });
     });
 });
