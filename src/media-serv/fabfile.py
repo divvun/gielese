82d236c55249955e27dbbad7c230f40cfe570782
Ryan Johnson <ryan.txanson@gmail.com>
2013-09-22 19:40:00 +0000

    Automate deployment tasks
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/aajege@81208 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ src/media-serv/fabfile.py
@@ -0,0 +1,79 @@
+# -*- encoding: utf-8 -*-
+import os
+import sys
+
+from fabric.api import local, task, cd, settings, abort, run
+from fabric.contrib.console import confirm
+
+# @task
+# def init():
+#     """Set up the project and environment.
+# 
+#     Install dependencies and create a symlink from an
+#     external media storage to Hyde's media folder for easier
+#     development and deployment.
+#     """
+#     local("pip install -r requirements.txt")
+#     if not os.path.exists(EXTERNAL_MEDIA_PATH):
+#         external_media_path = os.environ.get("EXTERNAL_MEDIA_PATH")
+#         if external_media_path is None:
+#             _print_and_exit("EXTERNAL_MEDIA_PATH environment variable not set.")
+#         local("ln -s {0} {1}".format(external_media_path,
+#             EXTERNAL_MEDIA_PATH))
+#     else:
+#         print("A link to an external media already exists.")
+
+@task
+def reinstall_db():
+    """Wipe the db, and reinstall"""
+    local("mv media_serv.db media_serv.db.bak")
+    local("sh install_db.sh")
+
+@task
+def rsync_svn():
+    local("cd ../ && sh rsync_the_things.sh")
+
+staging_remote_host_and_path = os.environ.get("GTLAB_AAJEGE_STAGING_HOST")
+if staging_remote_host_and_path is None:
+    _print_and_exit("GTLAB_AAJEGE_STAGING_HOST environment variable not set.")
+
+@task
+def update_target_envs():
+    host, _, path = staging_remote_host_and_path.partition(':')
+
+    media_db_path = path + '/src/media-serv/'
+    client_path = path + '/src/sma-client/'
+
+    with cd(path):
+        run("svn up")
+
+        with cd(media_db_path):
+            local("pip install -r requirements.txt")
+
+        with cd(client_path):
+            local("npm update")
+
+@task
+def deploy():
+    host, _, path = staging_remote_host_and_path.partition(':')
+
+    media_db_path = path + '/src/media-serv/'
+    client_path = path + '/src/sma-client/'
+
+    with cd(path):
+        run("svn up")
+
+        # reinstall db
+        with cd(media_db_path):
+            run("mv media_serv.db media_serv.db.bak")
+            run("sh install_db.sh")
+
+        # recompile client
+        with cd(client_path):
+            run("npm update")
+            run("brunch build")
+
+        # hup hup hup
+        with cd(media_db_path):
+            run("kill -HUP `cat pidfile`")
+
