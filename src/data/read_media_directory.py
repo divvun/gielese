882c9272ee0a5d3a8e3913271838ccb37c919142
Ryan Johnson <ryan.txanson@gmail.com>
2013-10-29 22:58:49 +0000

    xml output
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/aajege@82923 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/data/read_media_directory.py
+++ src/data/read_media_directory.py
@@ -59,12 +59,15 @@ from image size?
 TODO: merge in stuff from make_xml, including an output directory, which
 then copies and transforms files to a format compatible with Gielese
 
+TODO: categories.info + category.info
+
 Usage:
   read_media_directory.py read concepts <media_dir> [options]
   read_media_directory.py read categories <media_dir> [options]
   read_media_directory.py -h | --help
   read_media_directory.py show example concepts.info
   read_media_directory.py show example concept.info
+  read_media_directory.py show example categories.info
   read_media_directory.py show example category.info
 
 Options:
@@ -78,11 +81,51 @@ import yaml
 import simplejson as json
 import docopt
 
+import lxml.etree
+from lxml.builder import ElementMaker
+from lxml.etree import CDATA
+from lxml import etree
+
 concepts_info_example = """
 semantics:
   - FACE
   - BODY
 """
+
+# things like this should really happen at a different phase. maybe
+# build in conversion to an install process or something
+
+# concepts_media_transforms = """
+# media_transforms:
+#   - size: "orig"
+#     target:
+#       size: "medium"
+#       device: "tablet"
+#       max_width: 350
+#       max_height: 350
+#   - size: "orig"
+#     target:
+#       size: "small"
+#       device: "mobile"
+#       max_width: 200
+#       max_height: 200
+# 
+# media_matching:
+#   - file_pattern: "(small)"
+#     apply_attributes:
+#       size: "small"
+#       device: "mobile"
+# 
+#   - file_pattern: "(medium)"
+#     apply_attributes:
+#       size: "medium"
+#       device: "tablet"
+# 
+#   - file_pattern: "(orig)"
+#     apply_attributes:
+#       size: "original"
+#       device: ""
+# """
 
 concept_info_example = u"""
 lemma: ååredæjja
@@ -94,17 +137,25 @@ translations:
     swe: ansikt
 """
 
+# TODO: include media, or assume media structure
+categories_info_example = u"""
+main_menu: true
+"""
+
 category_info_example = u"""
-TODO
+category: "BODYPART"
+name: "Ååredæjja"
+semantics:
+  - "BODYPART"
 """
 
 def extra_doc(arguments):
-	if arguments.get('concepts.info', False):
-		print >> sys.stdout, concepts_info_example.encode('utf-8')
-	if arguments.get('concept.info', False):
-		print >> sys.stdout, concept_info_example.encode('utf-8')
-	if arguments.get('category.info', False):
-		print >> sys.stdout, concept_info_example.encode('utf-8')
+    if arguments.get('concepts.info', False):
+        print >> sys.stdout, concepts_info_example.encode('utf-8')
+    if arguments.get('concept.info', False):
+        print >> sys.stdout, concept_info_example.encode('utf-8')
+    if arguments.get('category.info', False):
+        print >> sys.stdout, concept_info_example.encode('utf-8')
 
 def find_directories_with(path, filename):
     """ Search a path returning directories that contain a filename.
@@ -222,7 +273,6 @@ def read_concepts_directory(concept_dir):
 
     return concepts
 
-
 def walk_for_concepts_sets(concept_path):
     """ Find all the directories that are marked as being concepts
     directories: containing a `concepts.info` file.
@@ -239,7 +289,94 @@ def walk_for_concepts_sets(concept_path):
     return concepts
 
 def concepts_to_xml(concepts):
-    pass
+
+    E = ElementMaker()
+
+    def create_entry(concept, rename_files=False, verbose=False):
+
+        def wrapCDATA(str):
+            return "<![CDATAA[%s]]>" % str
+
+        XML_LANG = "{http://www.w3.org/XML/1998/namespace}lang"
+
+        e, lg, mg = E.e, E.lg, E.mg
+        t, tg = E.t, E.tg
+
+        l = E.l
+
+        media = E.media
+        img, path_node, sound = E.img, E.path, E.sound
+
+        tgs = []
+
+        _semantics = concept.get('semantics', [])
+        lemma = concept.get('lemma', False)
+        translations = concept.get('translations', [])
+
+        _meds = concept.get('media', {})
+        _images = _meds.get('images', False)
+        _audio = _meds.get('audio', False)
+
+        lg_node = lg(l(lemma))
+
+        def rename_file(_p):
+            if rename_files:
+                return adjust_file_name(_sma, _p, rename_files)
+            else:
+                return _p
+
+        semantics = []
+        if _semantics:
+            for _sem in _semantics:
+                semantics.append(E.sem(**{'class':_sem}))
+
+        semantics = E.semantics(*semantics)
+
+        for tgroup in translations:
+            _tg = []
+            for lang, translation in tgroup.iteritems():
+                _tg.append( tg( t(translation), {XML_LANG: lang}))
+            tgs.extend(_tg)
+
+        medias = []
+        if _images:
+            images = []
+            for i in _images:
+
+                style_kwargs = {}
+                if i.get('size', False):
+                    style_kwargs['size'] = i['size']
+                if i.get('device', False):
+                    style_kwargs['device'] = i['device']
+
+                file_path = rename_file(i.get('path'))
+                _p = path_node()
+                _p.text = CDATA(file_path)
+
+                images.append(
+                    E.image(_p, style_kwargs)
+                )
+
+            medias.append(E.images(*images))
+
+        if _audio:
+            audio = []
+            for i in _audio:
+                _m = path_node()
+                _m.text = CDATA(rename_file(i.get('path')))
+                audio.append(sound(_m))
+            medias.append(E.sounds(*audio))
+
+        meaning_group = mg(semantics, *tgs)
+        e_node = e(lg_node, media(*medias), meaning_group)
+
+        return e_node
+
+    _r = E.r
+    nodes = _r(*map(create_entry, concepts))
+
+    return etree.tostring(nodes, pretty_print=True, encoding=unicode)
+
 
 def concepts_to_json(concepts):
     return json.dumps(concepts, indent=' ' * 2)
@@ -247,7 +384,6 @@ def concepts_to_json(concepts):
 def read_concepts(arguments):
     """ Walk the concepts directories for concepts, and output them to
         JSON or XML.
-
     """
 
     _cwd = os.getcwd()
@@ -258,11 +394,11 @@ def read_concepts(arguments):
         media_dir = media_dir.replace(common, '.')
 
     concept_dir = os.path.join(media_dir, 'concepts')
-    # category_dir = os.path.join(media_dir, 'categories')
 
     concepts = walk_for_concepts_sets(concept_dir)
 
     if arguments.get('--output', False):
+
         if arguments.get('--output') == 'JSON':
             serializer = concepts_to_json
         if arguments.get('--output') == 'XML':
@@ -271,12 +407,38 @@ def read_concepts(arguments):
         serialized = serializer(concepts)
         print >> sys.stdout, serialized
 
+def read_categories(arguments):
+
+    # TODO: consider subcategories -- need to find out if one category
+    # is contained by another
+
+    _cwd = os.getcwd()
+    media_dir = os.path.join(_cwd, arguments.get('<media_dir>'))
+
+    if not arguments.get('--absolute-paths', False):
+        common = os.path.commonprefix([_cwd, media_dir])
+        media_dir = media_dir.replace(common, '.')
+
+    categories = walk_for_categories_sets(concept_dir)
+
+    if arguments.get('--output', False):
+
+        if arguments.get('--output') == 'JSON':
+            serializer = concepts_to_json
+        if arguments.get('--output') == 'XML':
+            serializer = concepts_to_xml
+
+        serialized = serializer(categories)
+        print >> sys.stdout, serialized
+
 def main():
     arguments = docopt.docopt(__doc__, version='Media parser 0.1.0')
 
     if arguments.get('read', False):
         if arguments.get('concepts', False):
             read_concepts(arguments)
+        if arguments.get('categories', False):
+            read_categories(arguments)
 
     if arguments.get('show', False):
         if arguments.get('example', False):
