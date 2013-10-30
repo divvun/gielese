1849b25657f6eb18ee3e9962070987ac70bfe7e5
Ryan Johnson <ryan.txanson@gmail.com>
2013-10-30 23:13:07 +0000

    update install script
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/aajege@82978 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/media-serv/install_db.sh
+++ src/media-serv/install_db.sh
@@ -2,12 +2,11 @@
 
 rm data/*.json
 python manage.py init_db
-python manage.py install_media -f ../data/sma_media.xml
-python manage.py append_lexical_data -f ../data/n_smanob_test.xml
-python manage.py install_media -f ../data/heelsedh.xml
-python manage.py install_media -f ../data/beapmoeh.xml
-python manage.py install_media -f ../data/nature.animals.xml
-python manage.py install_media -f ../data/nature.world.xml
-python manage.py install_media -f ../data/phrases.xml
+python manage.py install_media -f ../data/xml/heelsedh.xml
+python manage.py install_media -f ../data/xml/beapmoeh.xml
+python manage.py install_media -f ../data/xml/nature.animals.xml
+python manage.py install_media -f ../data/xml/nature.world.xml
+python manage.py install_media -f ../data/xml/phrases.xml
+python manage.py install_media -f ../data/new_process.xml
 python manage.py prepare_json
 
