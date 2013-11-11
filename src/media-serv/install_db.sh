55336c71e72862d5578239da063b0e05750784b9
Ryan Johnson <ryan.txanson@gmail.com>
2013-11-11 23:21:16 +0000

    moving media to biggies, removing old categories from install process
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/aajege@83443 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- src/media-serv/install_db.sh
+++ src/media-serv/install_db.sh
@@ -6,8 +6,6 @@ python read_media_directory.py read categories static/media/ --output=JSON --ser
 python read_media_directory.py read concepts static/media/ --output=XML --server-media-uri=/ > data/concepts.tmp.xml
 
 python manage.py init_db
-python manage.py install_media -f ../data/xml/nature.animals.xml
-python manage.py install_media -f ../data/xml/nature.world.xml
 python manage.py install_media -f data/concepts.tmp.xml
 rm data/concepts.tmp.xml
 python manage.py prepare_json
